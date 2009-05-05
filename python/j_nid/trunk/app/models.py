3cdee0c9949ef70bcc6a7675d55532792a8a4d18
Chewpichai <Chewpichai@d95a5812-864a-4ace-ae67-56c9f0de05e7>
2009-05-05 17:11:06 +0000

    Updated order paid.

--- python/j_nid/trunk/app/models.py
+++ python/j_nid/trunk/app/models.py
@@ -11,11 +11,12 @@ class Person(models.Model):
     detail2 = models.TextField(null=True, blank=True, default='')
     type = models.PositiveIntegerField()
     
-    def __unicode__(self):
-        return u'%s' % self.name
-        
     class Meta:
         db_table = 'people'
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
 
 class BankAccount(models.Model):
     BANK_CHOICES = (
@@ -33,7 +34,8 @@ class BankAccount(models.Model):
         db_table = 'bank_accounts'
         unique_together = ('person', 'number')
         verbose_name = 'bank_account'
-        
+
+
 class PhoneNumber(models.Model):
     TYPE_CHOICES = (
         ('home', _('Home')),
@@ -50,7 +52,8 @@ class PhoneNumber(models.Model):
         db_table = 'phone_numbers'
         unique_together = ('person', 'number')
         verbose_name = 'phone_number'
-        
+
+
 class Payment(models.Model):
     person = models.ForeignKey(Person, related_name='payments')
     amount = models.DecimalField(max_digits=9, decimal_places=2)
@@ -59,17 +62,20 @@ class Payment(models.Model):
     
     class Meta:
         db_table = 'payments'
-        
+
+
 class ProductType(models.Model):
     name = models.CharField(max_length=255, unique=True)
     color = models.PositiveIntegerField()
     
-    def __unicode__(self):
-        return u'%s' % self.name
-    
     class Meta:
         db_table = 'product_types'
         verbose_name = 'product_type'
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
+
     
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -79,65 +85,63 @@ class Product(models.Model):
     price_per_unit = models.DecimalField(max_digits=9, decimal_places=2)
     is_sale = models.BooleanField()
     
-    def __unicode__(self):
-        return u'%s: %s' % (self.type, self.name)
-    
     class Meta:
         db_table = 'products'
         unique_together = ('name', 'type')
-        
+
+    def __unicode__(self):
+        return u'%s: %s' % (self.type, self.name)
+
+
 class Order(models.Model):
-    STATUS_OPTIONS = (
-        (-1, _('Canceled')),
-        (0, _('Outstanding')),
-        (1, _('Paid'))
-    )
-    
     person = models.ForeignKey(Person, related_name='orders')
     notation = models.TextField(null=True, blank=True, default='')
-    status = models.IntegerField(choices=STATUS_OPTIONS)
+    paid_total = models.DecimalField(max_digits=9, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
-    
-    def __unicode__(self):
-        return u'Order for %s' % self.person.name
     
     class Meta:
         db_table = 'orders'
-        
+
+    def __unicode__(self):
+        return u'Order for %s' % self.person.name   
+
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='order_items')
     product = models.ForeignKey(Product, related_name='order_items')
     cost_per_unit = models.DecimalField(max_digits=9, decimal_places=2)
-    price_per_unit = models.DecimalField(max_digits=7, decimal_places=2)
+    price_per_unit = models.DecimalField(max_digits=9, decimal_places=2)
     unit = models.PositiveIntegerField()
-    
-    def __unicode__(self):
-        return u'OrderItem %s' % self.product.name
     
     class Meta:
         db_table = 'order_items'
         verbose_name = 'order_item'
-		
+
+    def __unicode__(self):
+        return u'OrderItem %s' % self.product.name
+
+
 class Supply(models.Model):
     person = models.ForeignKey(Person, related_name='supplies')
     notation = models.TextField(null=True, blank=True, default='')
     created = models.DateTimeField(auto_now_add=True)
     
-    def __unicode__(self):
-        return u'Supply for %s' % self.person.name
-    
     class Meta:
         db_table = 'supplies'
-        
+
+    def __unicode__(self):
+        return u'Supply for %s' % self.person.name
+
+
 class SupplyItem(models.Model):
     supply = models.ForeignKey(Order, related_name='supply_items')
     product = models.ForeignKey(Product, related_name='supply_items')
-    price_per_unit = models.DecimalField(max_digits=7, decimal_places=2)
+    price_per_unit = models.DecimalField(max_digits=9, decimal_places=2)
     unit = models.PositiveIntegerField()
-    
-    def __unicode__(self):
-        return u'SupplyItem %s' % self.product.name
     
     class Meta:
         db_table = 'supply_items'
         verbose_name = 'supply_item'
+
+    def __unicode__(self):
+        return u'SupplyItem %s' % self.product.name
--- python/j_nid/trunk/app/sql/order.sql
+++ python/j_nid/trunk/app/sql/order.sql
@@ -1,4 +1,4 @@
-INSERT INTO orders(person_id, notation, status, created) VALUES(1, '', 0, '2009-01-12 08:30:22');
-INSERT INTO orders(person_id, notation, status, created) VALUES(1, '', 1, '2009-01-12 12:30:00');
-INSERT INTO orders(person_id, notation, status, created) VALUES(1, '', 0, '2009-01-15 08:30:22');
-INSERT INTO orders(person_id, notation, status, created) VALUES(2, '', 0, '2009-01-15 08:30:22');
+INSERT INTO orders(person_id, notation, paid_total, created) VALUES(1, '', 1347.0, '2009-01-12 08:30:22');
+INSERT INTO orders(person_id, notation, paid_total, created) VALUES(1, '', 180.0, '2009-01-12 12:30:00');
+INSERT INTO orders(person_id, notation, paid_total, created) VALUES(1, '', 110.0, '2009-01-15 08:30:22');
+INSERT INTO orders(person_id, notation, paid_total, created) VALUES(2, '', 110.0, '2009-01-15 08:30:22');
