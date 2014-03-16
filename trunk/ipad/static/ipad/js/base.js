af263b323699b01116b22152d344275afab1ddfc
chewpichai <chewpichai@d95a5812-864a-4ace-ae67-56c9f0de05e7>
2014-03-16 05:26:02 +0000

    updated ipad order form.

--- trunk/ipad/static/ipad/js/base.js
+++ trunk/ipad/static/ipad/js/base.js
@@ -1,7 +1,7 @@
 var dialog_instance;
 
 $(function() {
-	$('#add-product-btn').click(addProductBtnClick);
+  $('#add-product-btn').click(addProductBtnClick);
 
   $('body').on('click', '.product-list li a', productClick);
 
@@ -10,12 +10,17 @@ $(function() {
   $('.order-form').on('click', 'a[href=#price]', priceClick);
   $('.order-form').on('click', 'a.remove-product-btn', removeBtnClick);
 
-  $('select[name=customer]').combobox();
+  var current_hour = (new Date()).getHours();
+  $('select[name=customer]').val(++current_hour).change(customerChange).change().combobox();
+
+  $('#order-submit-btn').click(orderSubmitClick);
+
+  $('#id-note').val('').data('baskets', {});
 });
 
 
 function initProductTypeList() {
-  var width = 1;
+  var width = 0;
 
   $('.product-type-list li').each(function(i, elm) {
     width += $(elm).outerWidth(true);
@@ -69,23 +74,32 @@ function addProduct(elm, is_pledge) {
       name = $(elm).text(),
       unit = parseFloat($(elm).attr('unit')),
       price_per_unit = parseFloat($(elm).attr('price-per-unit')),
+      cost_per_unit = parseFloat($(elm).attr('cost-per-unit')),
       total = unit * price_per_unit,
       $existed_tr = $('.order-form tbody tr[product-id=' + product_id + '][product-type-id=' + product_type_id + ']'),
       output = [];
 
   if (product_type_id == 'basket' && !is_pledge) {
     var note_txt = $('#id-note').val(),
-        re = new RegExp(name + ' x \\d+', 'm'),
+        re = new RegExp(RegExp.quote(name) + ' x \\d+', 'm'),
         existed = re.exec(note_txt),
-        qty;
+        qty,
+        basket = {
+          is_basket: true,
+          price_per_unit: price_per_unit,
+          unit: unit,
+          product_id: product_id
+        };
 
     if (existed) {
       qty = parseInt(existed[0].split(' x ')[1]);
       qty++;
       note_txt = note_txt.replace(existed[0], name + ' x ' + qty);
+      $('#id-note').data('baskets')[name]['unit'] = qty;
     } else {
       if (note_txt.length > 0) note_txt += '\n';
       note_txt += name + ' x 1';
+      $('#id-note').data('baskets')[name] = basket;
     }
 
     $('#id-note').val(note_txt);
@@ -105,7 +119,7 @@ function addProduct(elm, is_pledge) {
     $existed_tr.find('a[href=#unit]').text(unit);
     $existed_tr.find('.total').text(unit * price);
   } else {
-    output.push('<tr product-type-id="' + product_type_id + '" product-id="' + product_id + '">');
+    output.push('<tr product-type-id="' + product_type_id + '" product-id="' + product_id + '" cost-per-unit="' + cost_per_unit + '">');
     
     if (product_type_id == 'basket') output.push('<td>0</td>');
     else output.push('<td><a href="#qty">1</a></td>');
@@ -117,18 +131,20 @@ function addProduct(elm, is_pledge) {
     output.push('</tr>');
 
     $(output.join('')).appendTo('.order-form tbody').addClass('animated pulse').swipe({
+      allowPageScroll: 'vertical',
       swipe: function(event, direction, distance, duration, fingerCount) {
-        console.log(direction);
         var $tr = $(event.currentTarget);
 
         if (direction == 'left') {
           $tr.find('.remove-product-btn-wrapper').show();
           $tr.find('.remove-product-btn').addClass('animated bounceInRight');  
         } else if (direction == 'right') {
-          $tr.find('.remove-product-btn').addClass('animated bounceOutRight').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $tr.find('.remove-product-btn-wrapper').hide();
-            $(this).removeClass('animated bounceOutRight bounceInRight');
-          });
+          if ($tr.find('.remove-product-btn').hasClass('bounceInRight')) {
+            $tr.find('.remove-product-btn').addClass('animated bounceOutRight').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+              $tr.find('.remove-product-btn-wrapper').hide();
+              $(this).removeClass('animated bounceOutRight bounceInRight');
+            });
+          }
         }
       },
     });
@@ -166,7 +182,10 @@ function quantityClick() {
         updateSummary();
         dialog.close();
       }
-    }]
+    }],
+    onshow: function(dialog) {
+      dialog.getModalDialog().swipe({swipe: numberSwipe});
+    }
   });
   return false;
 }
@@ -200,7 +219,10 @@ function unitClick() {
         updateSummary();
         dialog.close();
       }
-    }]
+    }],
+    onshow: function(dialog) {
+      dialog.getModalDialog().swipe({swipe: numberSwipe});
+    }
   });
   return false;
 }
@@ -230,7 +252,10 @@ function priceClick() {
         updateSummary();
         dialog.close();
       }
-    }]
+    }],
+    onshow: function(dialog) {
+      dialog.getModalDialog().swipe({swipe: numberSwipe});
+    }
   });
   return false;
 }
@@ -250,6 +275,8 @@ function updateSummary() {
   });
 
   $td.html(summary + '<br/><br/>' + quantity);
+  $('.payment-dialog .summary').html(summary);
+  $('.payment-dialog .quantity').html(quantity);
 }
 
 
@@ -284,3 +311,93 @@ function removeBtnClick() {
   });
   return false;
 }
+
+
+function customerChange() {
+  var customer_id = this.value,
+      outstanding = $(this).find('option[value=' + customer_id + ']').data('outstanding');
+  
+  $('.outstanding').html(Math.abs(outstanding));
+  $('.outstanding').removeClass('red');
+
+  if (outstanding < 0) {
+    $('.outstanding').addClass('red');
+  }
+}
+
+
+function numberSwipe(event, direction, distance, duration, fingerCount) {
+  var $input = $(event.currentTarget).find('input[type=number]'),
+      num = $input.val();
+  
+  if (direction == 'up') {
+    $input.val(++num);
+  } else if (direction == 'down') {
+    $input.val(--num);
+  }
+}
+
+
+function orderSubmitClick() {
+  var data = {
+        person: $('[name=customer]').val(),
+        order_items: [],
+        notation: $('#id-note').val()
+      };
+
+  $('tr[product-id]').each(function(i, elm) {
+    var $tr = $(elm),
+        item = {
+          is_basket: $tr.attr('product-type-id') == 'basket',
+          price_per_unit: parseFloat($tr.find('a[href=#price]').text()),
+          unit: parseInt($tr.find('a[href=#unit]').text()),
+          product: $tr.attr('product-id'),
+          cost_per_unit: parseFloat($tr.attr('cost-per-unit')),
+          is_deposit: true
+        };
+    data.order_items.push(item);
+  });
+
+  if (data.order_items.length == 0) return;
+
+  $.each($('#id-note').data('baskets'), function(name, basket) {
+    var re = new RegExp(RegExp.quote(name) + ' x (\\d+)', 'm'),
+        match = re.exec($('#id-note').val());
+    if (match) {
+      var item = {
+            is_basket: true,
+            price_per_unit: parseFloat(basket.price_per_unit),
+            unit: parseInt(match[1]),
+            product: basket.product_id,
+            is_deposit: false
+          };
+      data.order_items.push(item);
+    }
+  });
+
+  dialog_instance = BootstrapDialog.show({
+    title: 'ชำระเงิน',
+    message: $('.payment-dialog').clone(),
+    buttons: [{
+      label: 'บันทึก',
+      action: function(dialog) {
+        data.paid = parseFloat($(dialog.getMessage()).find('input[name=payment]').val());
+        $.post('/api/orders/create/', JSON.stringify(data), function(response) {
+          // dialog.close();
+          location.reload();
+        });
+      }
+    }],
+    onshow: function(dialog) {
+      if (data.person <= 24) {
+        var summary = $(dialog.getMessage()).find('.summary').html();
+        $(dialog.getMessage()).find('input[name=payment]').val(summary);
+      }
+    }
+  });
+}
+
+
+RegExp.quote = function(str) {
+  return (str+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+};
